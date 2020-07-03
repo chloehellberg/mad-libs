12d4f2cda7a45a8d1fe7ce4d9e4815bf38076ec0
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-07-02 18:43:36 -0700

    bug fix on person1 class

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
-  console.log("Script executing!");
   $("#formOne").submit(function() {
     event.preventDefault();
-    console.log("Submit form successfully reached.");
     const person1Input = $("input#person1").val();
     const person2Input = $("input#person2").val();
     const animalInput= $("input#animal").val();
@@ -10,7 +8,7 @@ $(document).ready(function() {
     const verbInput = $("input#verb").val();
     const nounInput = $("input#noun").val();
 
-    $("person1").text(person1Input);
+    $(".person1").text(person1Input);
     $(".person2").text(person2Input);
     $(".animal").text(animalInput);
     $(".exclamation").text(exclamationInput);
